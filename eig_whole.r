b5d27c1bd25b8f456811084b70ad860589348fcb
NuriddinovMA <admin@t03c005.icg.sbras.ru>
2020-08-12 13:26:21 +0700

    no --args

---
+++ eig_whole.r
@@ -0,0 +1,28 @@
+args <- commandArgs(trailingOnly = T)
+print(args)
+print("start")
+fname <- args[1]
+pc <- as.integer(args[2])
+res <- as.integer(args[3])
+chr <- args[4]
+st <- as.integer(as.integer(args[5])/res)+1
+fi <- as.integer(as.integer(args[6])/res)
+m.p <- c()
+
+m.s <- scan(fname, sep=' ')
+lnf <- as.integer( sqrt(length(m.s)) )
+m.m <- matrix(m.s,nrow=lnf,byrow=T)
+m.m[!is.finite(m.m)] <- 0
+
+m.pca <- princomp(m.m)
+m.pr <- predict(m.pca)[,pc]
+m.p <- m.pr
+ln <- length(m.p)
+
+pos1 <- seq.int(0,ln-1,1)
+pos1 <- (pos1 + st)*res
+pos2 <- pos1 + res - 1
+t.out <- format(data.frame(chr,pos1,pos2,m.p),trim=TRUE,scientific=FALSE)
+fout <- paste0(fname, '.whole.pc', pc, '.eig.bedGraph')
+
+write.table(file=fout,t.out,dec=",",col.names=F,row.names=F,sep='\t',quote=FALSE)
